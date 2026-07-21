f2a11df0d371678bb6b3a489eb5983204d6d15b9
SripujaAdigarlaa <sripujaadigarla@gmail.com>
2026-07-21 12:45:08 +0530

    Initial commit

---
+++ week-1/annotations1.js
@@ -0,0 +1,12 @@
+"use strict";
+// Variable Type Annotations
+let studentName = "Nissy";
+let rollNumber = 4549;
+let isPresent = true;
+// Function Type Annotations
+function displayStudent(name, roll, present) {
+    console.log("Student Name:", name);
+    console.log("Roll Number:", roll);
+    console.log("Present:", present);
+}
+displayStudent(studentName, rollNumber, isPresent);
---
+++ week-4/gen3.js
@@ -0,0 +1,6 @@
+"use strict";
+function printName(obj) {
+    console.log("Name:", obj.name);
+}
+printName({ name: "Nissy", age: 20 });
+printName({ name: "Rahul" });
